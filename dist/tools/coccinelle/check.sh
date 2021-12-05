e87ac2a93ce13611ce366b70c759002c0b03ea8f
luisan00 <luisan00@hotmail.com>
2021-12-05 14:01:03 +0100

    boards: add m4a-24g

--- dist/tools/coccinelle/check.sh
+++ dist/tools/coccinelle/check.sh
@@ -6,7 +6,7 @@
 # General Public License v2.1. See the file LICENSE in the top level
 # directory for more details.
 
-: "${RIOTBASE:=$(cd $(dirname $0)/../../../; pwd)}"
+: "${RIOTBASE:=$(cd $(dirname $0)/../../../ || exit; pwd)}"
 cd $RIOTBASE
 
 : "${RIOTTOOLS:=${RIOTBASE}/dist/tools}"
